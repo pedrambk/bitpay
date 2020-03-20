2d07e181a3add2e0ecbb477e582c091daddd43c9
pedrambirack <pedrambirack@gmail.com>
2020-03-20 13:17:42 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+var RSBP_CONFIG = {
+  "system": {
+    "urlConfig": true, // if 'true', load the the configuration from the URL
+                       // query string, else load it from RSBP_CONFIG.payee
+  },
+  "payee": {
+    "name": "pedram",
+    "address": "1NWYC2iqCcuZeoN4oTNbNeCWMxHjP7hjyJ",
+    "currency": "BTC",
+    "discount": 0 // in percent
+  }
+};
